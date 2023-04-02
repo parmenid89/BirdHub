af35646a49e76f84eb8c8dfe33da1ec6956c6312
Daniil <Daniil.postoyuk@gmail.com>
2023-04-02 23:51:39 +0300

    first commit

---
+++ dist/script.js
@@ -0,0 +1,14 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+$(document).ready(function () {
+  $('.conditions__slider').slick({
+    arrows: false,
+    dots: true
+  });
+});
+/******/ })()
+;
+//# sourceMappingURL=script.js.map
---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function () {
+    $('.conditions__slider').slick({
+        arrows: false,
+        dots: true
+    });
+});
